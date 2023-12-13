b5e872b24931d73c397ed63c931dd0ffa5bcc46c
gxvl <geraldo.goncalves@ufrpe.br>
2023-12-13 01:20:04 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro
+          name="Takai Gonçalves Lima"
+          paragraph="Oi, meu nome é Takai, tenho 8 anos e sou um Akita. Infelizmente não sei ler tampouco programar, mas minhas habilidades são as seguintes: "
+        />
+        <SkillList></SkillList>
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="takai.jpg" alt="foto-avatar" />;
+}
+
+function Intro(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.paragraph}</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Correr" emoji="🏃" color="cyan" />
+      <Skill skill="Brincar" emoji="🪀" color="yellow" />
+      <Skill skill="Mimir" emoji="😴" color="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div style={{ backgroundColor: props.color }} className="skill">
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
